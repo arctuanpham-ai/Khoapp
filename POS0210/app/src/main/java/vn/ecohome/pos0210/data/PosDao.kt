0e5cbbf82aea63b8641153cbaf0867321820b765
arctuanpham-ai <arc.tuanpham@gmail.com>
2026-09-10 02:51:32 +0700

    Harden kitchen print job state transitions

--- POS0210/app/src/main/java/vn/ecohome/pos0210/data/PosDao.kt
+++ POS0210/app/src/main/java/vn/ecohome/pos0210/data/PosDao.kt
@@ -38,6 +38,7 @@ import kotlinx.coroutines.flow.Flow
 @Query("SELECT * FROM PurchaseCategoryEntity ORDER BY sortOrder,name") suspend fun allPurchaseCategoriesSnapshot():List<PurchaseCategoryEntity>
 @Query("SELECT * FROM PurchaseItemEntity WHERE purchaseId=:purchaseId") fun purchaseItems(purchaseId:String):Flow<List<PurchaseItemEntity>>
 @Query("SELECT * FROM PrintJobEntity ORDER BY createdAt DESC") fun printJobs():Flow<List<PrintJobEntity>>
+@Query("SELECT * FROM PrintJobEntity WHERE batchId=:batchId AND type=\'KITCHEN\' LIMIT 1") suspend fun kitchenPrintJob(batchId:String):PrintJobEntity?
 @Query("SELECT * FROM AuditEventEntity ORDER BY occurredAt DESC LIMIT 100") fun audits():Flow<List<AuditEventEntity>>
 @Query("SELECT * FROM AppSettingEntity") fun settings():Flow<List<AppSettingEntity>>
 @Query("SELECT * FROM AreaEntity ORDER BY sortOrder,name") suspend fun allAreasSnapshot():List<AreaEntity>
